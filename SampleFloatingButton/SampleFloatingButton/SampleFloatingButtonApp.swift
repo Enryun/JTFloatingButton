9466044efcb9207ccef3d9dd1c09e65b0a0c0142
JamesThang <jamesthang1996@gmail.com>
2025-09-28 23:51:57 +0700

    add sample project

---
+++ SampleFloatingButton/SampleFloatingButton/SampleFloatingButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleFloatingButtonApp.swift
+//  SampleFloatingButton
+//
+//  Created by James Thang on 28/9/25.
+//
+
+import SwiftUI
+
+@main
+struct SampleFloatingButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
