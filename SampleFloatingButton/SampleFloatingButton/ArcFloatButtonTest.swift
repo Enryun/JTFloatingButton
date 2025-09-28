8693745608e28176f46758673a282f3d1363420f
JamesThang <jamesthang1996@gmail.com>
2025-09-29 01:03:00 +0700

    fix demo app

--- SampleFloatingButton/SampleFloatingButton/ArcFloatButtonTest.swift
+++ SampleFloatingButton/SampleFloatingButton/ArcFloatButtonTest.swift
@@ -8,7 +8,7 @@
 import JTFloatingButton
 import SwiftUI
 
-struct FloatActionViewTest: View {
+struct ArcFloatButtonTest: View {
     
     private let actions = [
         FloatingAction(image: Image(systemName: "tray.full.fill"), tint: .red, background: .white) {
--- SampleFloatingButton/SampleFloatingButton/SampleFloatingButtonApp.swift
+++ SampleFloatingButton/SampleFloatingButton/SampleFloatingButtonApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SampleFloatingButtonApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ArcFloatButtonTest()
         }
     }
 }
